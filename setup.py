9a27feef4f4128ae61b256442a14b8b3b492bd2b
Daniel Berliner <dan.berliner@sparkmeter.io>
2023-12-04 10:59:33 -0600

    Make setup.py compatible  with the buildroot/setuptools process

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
-from setuptools import setup
+#!/usr/bin/python
 
-# Load __version__ without importing it (avoids race condition with build)
-exec(open('terseparse/version.py').read())
+# Copyright (c) 2023, Sparkmeter Inc
+# All rights reserved.
+#
+
+"""Setup script for IntelHex."""
+
+import sys, glob
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from distutils.core import Command
+
+import terseparse, terseparse.__version__
+
+needs_pytest = set(['pytest', 'test', 'ptr']).intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 test_deps = [
     'pytest'
@@ -11,23 +26,24 @@ extras = {
     'test': test_deps
 }
 
-setup(name='terseparse',
+METADATA = dict(
+      name='terseparse',
       description='A minimal boilerplate, composeable wrapper for argument parsing',
+      version=terseparse.__version__.version_str,
       packages=['terseparse'],
-      version=__version__,
       url='https://github.com/jthacker/terseparse',
       download_url='https://github.com/jthacker/terseparse/archive/v{}.tar.gz'.format(__version__),
       author='jthacker',
       author_email='thacker.jon@gmail.com',
-      keywords=['argument', 'parsing'],
-      classifiers=[],
+      url='https://gitlab.com/sparkmeter/terseparse',
+      keywords='Argument Parsing',
+      license='MIT',
+      classifiers = [],
       install_requires=[
           'six >= 1.16'
           ],
       tests_require=test_deps,
-      setup_requires=[
-          'pytest-runner'
-          ],
+      setup_requires=pytest_runner,
       extras_require=extras,
       long_description="""
 How to Install
@@ -37,4 +53,12 @@ How to Install
 
     pip install terseparse
 
-""")
+"""
+)
+
+def main():
+    metadata = METADATA.copy()
+    return setup(**metadata)
+
+if __name__ == '__main__':
+    main()
---
+++ terseparse/__version__.py
@@ -0,0 +1,2 @@
+version_info = (1, 3, 1)
+version_str = '.'.join([str(i) for i in version_info])
